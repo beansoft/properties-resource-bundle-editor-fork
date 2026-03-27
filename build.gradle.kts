cafb3116ced07642033ff7f0681b9fa8532286b0
BeanSoft <beansoft@126.com>
2026-03-27 10:09:21 +0800

    Init repo

---
+++ build.gradle.kts
@@ -0,0 +1,64 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+
+plugins {
+  id("java")
+  id("org.jetbrains.kotlin.jvm") version "2.3.0"
+  id("org.jetbrains.intellij.platform") version "2.13.1"
+}
+
+group = "com.intellij.properties.bundle.editor"
+version = "261.0.0"
+
+repositories {
+  mavenCentral()
+  intellijPlatform {
+    defaultRepositories()
+  }
+}
+
+dependencies {
+  intellijPlatform {
+    intellijIdea("261.22158.182")
+    bundledPlugin("com.intellij.properties")
+
+    testFramework(TestFrameworkType.Platform)
+  }
+
+  testImplementation("junit:junit:4.13.2")
+}
+
+kotlin {
+  jvmToolchain(21)
+}
+
+java.sourceSets["main"].java {
+  srcDir("src")
+}
+
+java.sourceSets["main"].resources {
+  srcDir("resources")
+}
+
+java.sourceSets["test"].java {
+  srcDir("test")
+}
+
+intellijPlatform {
+  pluginConfiguration {
+    ideaVersion {
+      sinceBuild = "253"
+      untilBuild = "999"
+    }
+
+    changeNotes = ""
+  }
+}
+
+tasks {
+  // Set the JVM compatibility versions
+  withType<JavaCompile> {
+    sourceCompatibility = "21"
+    targetCompatibility = "21"
+  }
+}
